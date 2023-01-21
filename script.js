32ebbf2dbed1fbf4af719f22bbde79189c6ff73a
svetlanavinnikova <52204138+svetlanavinnikova@users.noreply.github.com>
2023-01-21 05:01:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(".clients__icon").click(function() {
+
+    if ($(".drop-down__text--clients").is(":hidden")) {
+    
+		    $(".drop-down__text--clients").slideDown("slow");
+		    $(".clients__icon").addClass("open");
+        
+    } else {
+        $(".drop-down__text--clients").hide("slow");
+	    $(".clients__icon").removeClass("open");
+    
+    }
+
+});
+
+$(".owners__icon").click(function() {
+
+    if ($(".owners__wrap").is(":hidden")) {
+    
+		    $(".owners__wrap").slideDown("slow");
+		    $(".owners__icon").addClass("open");
+        
+    } else {
+        $(".owners__wrap").hide("slow");
+	    $(".owners__icon").removeClass("open");
+    
+    }
+
+});
